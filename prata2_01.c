8e902126b85812620ace54ed5e60465489976f08
landauleo <bambie7@mail.ru>
2018-10-15 12:58:17 +0300

    2nd chapter's task

---
+++ prata2_01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+
+
+  printf("%s %s\n", "Leo", "Landau");
+  printf("%s\n%s\n", "Leo", "Landau");
+  printf("%s ", "Leo");
+  printf("%s\n", "Landau" );
+
+  return 0;
+}
