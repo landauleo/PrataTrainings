4974a6d34daa4f5244b648515ba3bb3d5d6c3be9
landauleo <bambie7@mail.ru>
2018-10-19 00:18:21 +0300

    4th chapter's task

---
+++ prata4_06.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <float.h>
+
+int main(void)
+{
+
+  float a = 1.0 / 3.0;
+  double b = 1.0 / 3.0;
+  printf(" float values: ");
+  printf("%.4f %.12f %.16f\n", a, a, a);
+  printf("double values: ");
+  printf("%.4f %.12f %.16f\n", b, b, b);
+  printf("FLT_DIG: %d\n", FLT_DIG);
+  printf("DBL_DIG: %d\n", DBL_DIG);
+
+  return 0;
+}
